8ac5f1cc27236f337d94788d63ead3015a1a883a
Lachlan Cresswell <lachy93@gmail.com>
2021-09-04 02:57:14 +0000

    Move to axios

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,12 @@
 import * as Papa from 'papaparse';
-import fetch from 'node-fetch';
-import request from 'request';
-import * as https from 'https';
 import * as fs from 'fs';
 import * as dotenv from 'dotenv';
 import ExposureSiteInfo from './Types/ExposureSiteInterface';
 import GeocodeInterface from './Types/GeocodeInterface';
 import VicDataInterface from './Types/VicDataInterface'
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 
-const envConfig = dotenv.config({ path: `${__dirname}/../../.env` });
+const envConfig = dotenv.config({ path: `${__dirname}/../../../.env` });
 if (envConfig.error) {
   throw envConfig.error;
 }
@@ -27,34 +25,19 @@ const DATA_VIC_HEADER = {
  * @param address Address string to find lat + long of
  * @returns Object containing lat + long
  */
-const getGeocodeLatLong = (address: string): Promise<{ longitude: number, latitude: number } | Error> => new Promise((resolve, reject) => {
-  const options = {
-    hostname: 'maps.googleapis.com',
-    port: 443,
-    path: `/maps/api/geocode/json?address=${address}&key=${process.env.GOOGLE_API_KEY}`,
+const getGeocodeLatLong = (address: string): Promise<{ lng: number, lat: number } | Error> => new Promise(async (resolve, reject) => {
+  const options: AxiosRequestConfig = {
+    baseURL: 'https://maps.googleapis.com',
+    url: `/maps/api/geocode/json?address=${address}&key=${process.env.GOOGLE_API_KEY}`,
     method: 'GET',
   };
 
-  const req = https.request(options, (response) => {
-    let data = '';
-    let sdata: GeocodeInterface;
+  const req: { data: GeocodeInterface } = await axios(options)
 
-    response.on('data', (chunk: string) => {
-      data += chunk;
-    });
-
-    response.on('end', () => {
-      sdata = JSON.parse(data);
-      resolve({
-        latitude: sdata.results[0].geometry.viewport.northeast.lat,
-        longitude: sdata.results[0].geometry.viewport.northeast.lng,
-      });
-    });
+  resolve({
+    lat: req.data.results[0].geometry.viewport.northeast.lat,
+    lng: req.data.results[0].geometry.viewport.northeast.lng,
   });
-  req.on('error', (e) => {
-    reject(e);
-  });
-  req.end();
 });
 
 /**
@@ -68,8 +51,8 @@ const parseSiteCsv = async (siteCsvData: [ExposureSiteInfo]): Promise<any> => {
 
     const latLong = await getGeocodeLatLong(address);
     if (!(latLong instanceof Error)) {
-      siteCsvData[i].latitude = latLong.latitude;
-      siteCsvData[i].longitude = latLong.longitude;
+      siteCsvData[i].latitude = latLong.lat;
+      siteCsvData[i].longitude = latLong.lng;
       processed.push(siteCsvData[i]);
     } else {
       throw (latLong);
@@ -81,17 +64,17 @@ const parseSiteCsv = async (siteCsvData: [ExposureSiteInfo]): Promise<any> => {
 };
 
 // Fetch exposure site .csv url from api.vic.gov.au and then fetch actual file
-fetch(DATA_VIC_URL, { method: 'GET', headers: DATA_VIC_HEADER }).then((response: { json: () => Promise<VicDataInterface> }) => {
-  response.json().then((vicDataResponse: VicDataInterface) => {
-    const csvUrl = vicDataResponse._embedded.resources[0]._links[1].href;
-    const parseStream = Papa.parse(Papa.NODE_STREAM_INPUT, { header: true });
+axios.get(DATA_VIC_URL, { headers: DATA_VIC_HEADER }).then(async (response: { data: VicDataInterface }) => {
+  const vicDataResponse = response.data
+  const csvUrl = vicDataResponse._embedded.resources[0]._links[1].href;
+  const parseStream = Papa.parse(Papa.NODE_STREAM_INPUT, { header: true });
 
-    const dataStream = request
-      .get(csvUrl)
-      .pipe(parseStream);
+  const csvRequest = await axios
+    .get(csvUrl, { responseType: 'stream' })
 
-    const csvData: any = [];
-    parseStream.on('data', (d) => csvData.push(d));
-    dataStream.on('finish', () => parseSiteCsv(csvData));
-  });
+  csvRequest.data.pipe(parseStream);
+
+  const csvData: any = [];
+  parseStream.on('data', (d) => csvData.push(d));
+  parseStream.on('finish', () => parseSiteCsv(csvData));
 });
