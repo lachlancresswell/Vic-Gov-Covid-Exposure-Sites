270ceaf63f46ce6b30a8b96bc6f5509b12b891db
Lachlan Cresswell <lachy93@gmail.com>
2021-09-06 04:40:20 +0000

    Style

--- client/src/app.ts
+++ client/src/app.ts
@@ -6,6 +6,7 @@ const SITE_PATH = '../../sites.json';
 let map: google.maps.Map;
 let siteJson: [ExposureSiteInfo];
 let markers: google.maps.Marker[] = [];
+let curInfoWindw: google.maps.InfoWindow;
 
 const updateMarkers = (json: [ExposureSiteInfo]): void => {
   const startDate = new Date(startDateElement.value);
@@ -20,13 +21,17 @@ const updateMarkers = (json: [ExposureSiteInfo]): void => {
     const dateStr = site.Exposure_date.split("/");
     var exposureDate = new Date(parseInt(dateStr[2]), parseInt(dateStr[1]) - 1, parseInt(dateStr[0]), 10);
 
-
     if (startDate.getTime() <= exposureDate.getTime() && endDate.getTime() >= exposureDate.getTime()) {
-      console.log(exposureDate)
+      let icon;
+      if (site.Advice_title.includes('Tier 1')) icon = 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'
+      else if (site.Advice_title.includes('Tier 2')) icon = 'http://maps.google.com/mapfiles/ms/icons/orange-dot.png'
+      else icon = 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
+
       const marker = new google.maps.Marker({
         position: { lat: site.latitude, lng: site.longitude },
         title: site.Site_title,
         map: map,
+        icon
       });
 
       const infowindow = new google.maps.InfoWindow({
@@ -46,6 +51,9 @@ const updateMarkers = (json: [ExposureSiteInfo]): void => {
       });
 
       marker.addListener("click", () => {
+        if (curInfoWindw) curInfoWindw.close();
+        curInfoWindw = infowindow;
+
         infowindow.open({
           anchor: marker,
           map,
@@ -75,10 +83,26 @@ const loadJSON = (path: string, callback: any) => {
   };
 }
 
+// Define as global
+(window as any).initMap = (): void => {
+  const melbourne = { lat: -37.8136, lng: 144.9631 };
+  map = new google.maps.Map(
+    document.getElementById("map") as HTMLElement,
+    {
+      zoom: 10,
+      center: melbourne,
+    }
+  );
+
+  loadJSON(SITE_PATH, updateMarkers);
+};
+
+
 const script = document.createElement("script");
 script.src = `https://maps.googleapis.com/maps/api/js?key=${keys}&callback=initMap&libraries=&v=weekly`;
 script.async = true;
 document.body.appendChild(script);
+
 const startDateElement = (document.getElementById('startDate') as HTMLInputElement);
 const endDateElement = (document.getElementById('endDate') as HTMLInputElement);
 
@@ -87,7 +111,7 @@ const todayStr = today.toISOString().split('T')[0];
 const minDate = new Date(
   today.getFullYear(),
   today.getMonth(),
-  (today.getDate() - 3)
+  (today.getDate() - 14)
 )
 const minDateStr = minDate.toISOString().split('T')[0];
 startDateElement.max = todayStr
@@ -102,19 +126,3 @@ startDateElement.addEventListener('change', (event) => {
 endDateElement.addEventListener('change', (e) => {
   updateMarkers(siteJson)
 });
-
-
-// Define as global
-(window as any).initMap = (): void => {
-  const melbourne = { lat: -37.8136, lng: 144.9631 };
-  // The map, centered at Uluru
-  map = new google.maps.Map(
-    document.getElementById("map") as HTMLElement,
-    {
-      zoom: 10,
-      center: melbourne,
-    }
-  );
-
-  loadJSON(SITE_PATH, updateMarkers);
-};
